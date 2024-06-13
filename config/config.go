76b83df57ed4de3d511224f968026e85085367e3
Ezvvin <pavel.klimov2012@ya.ru>
2024-06-13 11:36:59 +0300

    add_server

---
+++ config/config.go
@@ -0,0 +1,5 @@
+package config
+
+type servConfig struct {
+	Port string `default:7540`
+}
--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
 
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	handler := chi.NewRouter()
+
+	if err := http.ListenAndServe(servConfig.Port, handler); err != nil {
+		fmt.Printf("Ошибка при запуске сервера: %s", err.Error())
+		return
+	}
 }
