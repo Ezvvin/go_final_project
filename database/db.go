ffde385b7d88f95177d5266060d7ec541178fa8b
Ezvvin <pavel.klimov2012@ya.ru>
2024-06-14 11:13:21 +0300

    add_db_driver

---
+++ database/db.go
@@ -0,0 +1,17 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "modernc.org/sqlite"
+)
+
+func createDatabase() {
+	db, err := sql.Open("sqlite", "db_name.db")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer db.Close()
+}
---
+++ models/models.go
@@ -0,0 +1,11 @@
+package models
+
+const DatePattern string = "20060102"
+
+type Task struct {
+	ID      uint   `json:"id,string"` // автоинкрементный идентификатор
+	Date    string `json:"date"`      // дата задачи, которая будет хранится в формате YYYYMMDD или в Go-представлении 20060102;
+	Title   string `json:"title"`     // заголовок задачи;
+	Comment string `json:"comment"`   // комментарий к задаче;
+	Repeat  string `json:"repeat"`    // строковое поле не более 128 символов
+}
