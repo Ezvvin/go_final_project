edeee76d137a0975a2fadc41b87035b073e1d0bc
Pavel <pavel.klimov2012@ya.ru>
2024-06-16 20:06:16 +0300

    db

--- database/db.go
+++ database/db.go
@@ -3,12 +3,22 @@ package database
 import (
 	"database/sql"
 	"fmt"
+	"os"
 
+	"github.com/joho/godotenv"
 	_ "modernc.org/sqlite"
 )
 
-func createDatabase() {
-	db, err := sql.Open("sqlite", "db_name.db")
+func getDBFile() string {
+	err := godotenv.Load("local.env")
+	if err != nil {
+		fmt.Println("Не удалось найти .env")
+	}
+	return ":" + os.Getenv("TODO_DBFile")
+}
+
+func CreateDatabase() {
+	db, err := sql.Open("sqlite", getDBFile())
 	if err != nil {
 		fmt.Println(err)
 		return
